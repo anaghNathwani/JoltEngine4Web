8a07044694fd3affd60139c3bcf4a0e5281435ea
Anagh Nathwani <163938371+anaghNathwani@users.noreply.github.com>
2026-04-22 14:06:34 -0700

    Create src/index.js file with React DOM root creation

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const root = React.createRoot(document.getElementById('root'));
+root.render(<React.StrictMode><App /></React.StrictMode>);
